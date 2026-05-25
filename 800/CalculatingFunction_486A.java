f0188697319931a14c451c66559b8e54722e5b31
Ankit Singh <ankitsingh10a@gmail.com>
2026-05-26 00:05:31 +0530

    CP31 1901 solved

--- 800/CalculatingFunction_486A.java
+++ 800/CalculatingFunction_486A.java
@@ -1,29 +1,23 @@
-
-import java.math.BigInteger;
+ 
 import java.util.*;
-
+ 
 public class CalculatingFunction_486A {
     
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-
-
+ 
+ 
         long  n = sc.nextLong();
-
-        BigInteger x = BigInteger.valueOf(n);
-        
-        BigInteger odd = (((x+1)/2) * ((x+1)/2));
-
-        BigInteger ts=(n*(n+1))/2;
-
-        BigInteger even = ts- odd;
-        BigInteger result = even - odd;
-
+        long odd = (((n+1)/2) * ((n+1)/2));
+ 
+        long ts=(n*(n+1))/2;
+        long result = ts- (2 * odd);
+ 
        
-
+ 
         System.out.println(result);
-
+ 
         sc.close();
        
     }
